69c605c35ff9fd4367908978aee5c059e8a9d69b
ming9275 <edwardyau927@hotmail.com>
2014-01-16 11:55:25 -0800

    Signed-off-by: ming9275 <edwardyau927@hotmail.com>

---
+++ Assignment 3/Savitch_8thEd_Chap3_Prob1/main.cpp
@@ -0,0 +1,36 @@
+/* 
+ * File:   main.cpp
+ * Author: Kai Ming Yau
+ * Created on January 16, 2014, 11:38 AM
+ * Prob1
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins
+int main(int argc, char** argv) {
+
+    //Declare variables
+    char P, R, S, p, s, r, x, y;//P = paper, R = Rock, S = Scissor
+    
+    //type each letter
+    cout<<"This is Paper-rock-scissor game"<<endl;
+    cout<<"P = paper, R = Rock, S = Scissor"<<endl;
+    cout<<"Please the first player input a letter"<<endl;
+    cin>>X;
+    cout<<"Please the second player input a letter"<<endl;
+    cin>>y;
+    //Process
+    if (x==P||p);
+    
+    
+    
+    return 0;
+}
+
